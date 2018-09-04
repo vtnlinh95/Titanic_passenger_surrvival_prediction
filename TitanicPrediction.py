e82232a2f39eae84e2613258b2cf2f390bec8388
vtnlinh95 <jennylinh95@gmail.com>
2018-09-04 19:22:46 +0700

    using majority function, calculate the accuracy

--- TitanicPrediction.py
+++ TitanicPrediction.py
@@ -6,6 +6,15 @@ import seaborn as sns
 import random
 from titanic_visualizations import survival_stats
 
+# Class Node which will be used while classify a test-instance using the tree which was built earlier
+class Node():
+    value = ""
+    children = []
+
+    def __init__(self, val, dictionary):
+        self.value = val
+        if (isinstance(dictionary, dict)):
+            self.children = dictionary.keys()
 
 def accuracy_score(truth, pred, name):
     """ Returns accuracy score for input truth and predictions. """
@@ -37,85 +46,47 @@ def calculate_entropy(original_data, feature, value):
     return (pyes + pno) * pt
 
 
-def find_tree(root_entropy, original_data):
+def find_tree(original_data):
     count_survived = original_data[original_data['Survived'] == 1].shape[0]
     count_not_survived = original_data[original_data['Survived'] == 0].shape[0]
-    if count_survived == 0:
-        print "pure_leaf Survied"
-        return
-    if count_not_survived == 0:
+    if count_survived == 0 and count_not_survived != 0:
         print "pure_leaf Not Survied"
-        return
+        return 0
+    if count_not_survived == 0 and count_survived != 0:
+        print "pure_leaf Survied"
+        return 1
     list_features = list(original_data.columns.values)
     list_features.remove('Survived')
     max_feature = None
     max_gain = -1
-    max_entropy = 0
     for feature in list_features:
         list_values = original_data[feature].unique()
         entropy = 0
         for value in list_values:
             entropy += calculate_entropy(original_data, feature, value)
-        gain = root_entropy - entropy
+        gain = calculate_entropy(original_data, feature=None, value=None) - entropy
         if gain > max_gain:
             max_feature = feature
-            max_entropy = entropy
-    for value in original_data[max_feature].unique():
+            max_gain = gain
+    tree = {max_feature: {}}
+    for value in full_data[max_feature].unique():
         new_data = original_data.copy()
         new_data = new_data[new_data[max_feature] == value]
         new_data = new_data.drop(max_feature, axis=1)
         features = list(new_data.columns.values)
-        if len(features) == 1 and features[0] == 'Survived':
-            print "Can not decide"
+        if len(features) == 1 and features.__contains__('Survived'):
+            if count_not_survived > count_survived:
+                print "Not Survived"
+                tree[max_feature][value] = 0
+            else:
+                print "Survived"
+                tree[max_feature][value] = 1
             continue
         print max_feature
         print value
-        find_tree(max_entropy, new_data)
-    return
-
-
-def predict_for_passenger_in_thrities(data):
-    predictions = []
-    for index, passenger in data.iterrows():
-        if passenger['Pclass'] == 1:
-            if passenger['Sex'] == 'male':
-                if passenger['SibSp'] == 0:
-                    if passenger['Fare'] == 1:
-                        if passenger['Cabin'][:1] == 'B':
-                            predictions.append(1)
-                        elif passenger['Cabin'][:1] == 'E':
-                            predictions.append(0)
-                        elif passenger['Cabin'][:1] == 'A':
-                            predictions.append(1)
-                        else:
-                            predictions.append(random.randint(0, 1))
-                    elif passenger['Fare'] == '2':
-                        predictions.append(1)
-                    elif passenger['Fare'] == '6':
-                        predictions.append(0)
-                    else:
-                        predictions.append(random.randint(0, 1))
-                elif passenger['SibSp'] == 1:
-                    predictions.append(0)
-                else:
-                    predictions.append(random.randint(0, 1))
-            else:
-                predictions.append(0)
-        elif passenger['Pclass'] == 3:
-            predictions.append(0)
-        elif passenger['Pclass'] == 2:
-            if passenger['Sex'] == 'male':
-                if passenger['Embarked'] == 'C':
-                    predictions.append(1)
-                elif passenger['Embarked'] == 'S':
-                    predictions.append(0)
-                else:
-                    predictions.append(random.randint(0, 1))
-            else:
-                predictions.append(0)
-
-    # Return our predictions
-    return pd.Series(predictions)
+        subtree = find_tree(new_data)
+        tree[max_feature][value] = subtree
+    return tree
 
 # Load the dataset
 in_file = 'train.csv'
@@ -123,15 +94,18 @@ full_data = pd.read_csv(in_file)
 full_data = full_data.drop('PassengerId', axis=1)
 full_data = full_data.drop('Name', axis=1)
 full_data = full_data.drop('Ticket', axis=1)
+full_data = full_data.drop('Pclass', axis=1)
+full_data = full_data.drop('Age', axis=1)
+full_data = full_data.drop('Sex', axis=1)
 full_data = full_data.dropna()
-full_data.loc[operator.__and__(0.0 <= full_data.Age, full_data.Age <= 10.0), 'Age'] = 0
-full_data.loc[operator.__and__(11.0 <= full_data.Age, full_data.Age <= 20.0), 'Age'] = 1
-full_data.loc[operator.__and__(21.0 <= full_data.Age, full_data.Age <= 30.0), 'Age'] = 2
-full_data.loc[operator.__and__(31.0 <= full_data.Age, full_data.Age <= 40.0), 'Age'] = 3
-full_data.loc[operator.__and__(41.0 <= full_data.Age, full_data.Age <= 50.0), 'Age'] = 4
-full_data.loc[operator.__and__(51.0 <= full_data.Age, full_data.Age <= 60.0), 'Age'] = 5
-full_data.loc[operator.__and__(61.0 <= full_data.Age, full_data.Age <= 70.0), 'Age'] = 6
-full_data.loc[operator.__and__(71.0 <= full_data.Age, full_data.Age <= 80.0), 'Age'] = 7
+# full_data.loc[operator.__and__(0.0 <= full_data.Age, full_data.Age <= 10.0), 'Age'] = 0
+# full_data.loc[operator.__and__(11.0 <= full_data.Age, full_data.Age <= 20.0), 'Age'] = 1
+# full_data.loc[operator.__and__(21.0 <= full_data.Age, full_data.Age <= 30.0), 'Age'] = 2
+# full_data.loc[operator.__and__(31.0 <= full_data.Age, full_data.Age <= 40.0), 'Age'] = 3
+# full_data.loc[operator.__and__(41.0 <= full_data.Age, full_data.Age <= 50.0), 'Age'] = 4
+# full_data.loc[operator.__and__(51.0 <= full_data.Age, full_data.Age <= 60.0), 'Age'] = 5
+# full_data.loc[operator.__and__(61.0 <= full_data.Age, full_data.Age <= 70.0), 'Age'] = 6
+# full_data.loc[operator.__and__(71.0 <= full_data.Age, full_data.Age <= 80.0), 'Age'] = 7
 
 full_data.loc[operator.__and__(0.0 <= full_data.Fare, full_data.Fare <= 100.0), 'Fare'] = 1
 full_data.loc[operator.__and__(101.0 <= full_data.Fare, full_data.Fare <= 200.0), 'Fare'] = 2
@@ -144,13 +118,33 @@ full_data['Cabin'] = full_data['Cabin'].apply(lambda cabin : cabin[:1])
 
 original_data = full_data.sample(frac=0.7)
 test_data = full_data.drop(original_data.index)
-test_data = test_data[test_data['Age'] == 3]
+# test_data = test_data[test_data['Age'] == 3]
+tree = {}
 outcomes = test_data['Survived']
 original_data.head()
-root_entropy = calculate_entropy(original_data, feature=None, value=None)
-find_tree(root_entropy, original_data)
-# predictions = predict_for_passenger_in_thrities(test_data)
-# predictions.reset_index(drop=True)
-# outcomes.reset_index(drop=True)
-# print(accuracy_score(outcomes, predictions, "Passenger in Thirties"))
-arr1 = np.arra
+tree = find_tree(original_data)
+
+results = []
+for entry in test_data.iterrows():
+    tempDict = tree.copy()
+    result = ""
+    while (isinstance(tempDict, dict)):
+        root = Node(tempDict.keys()[0], tempDict[tempDict.keys()[0]])
+        tempDict = tempDict[tempDict.keys()[0]]
+        value = entry[1][root.value]
+        if (value in tempDict.keys()):
+            child = Node(value, tempDict[value])
+            result = tempDict[value]
+            tempDict = tempDict[value]
+        else:
+            result = "Null"
+            break
+    if result != "Null":
+        results.append(result)
+    else:
+        results.append(0)
+results = pd.Series(results)
+outcomes = test_data['Survived']
+results.reset_index(drop=True)
+outcomes.reset_index(drop=True)
+print(accuracy_score(outcomes, results, "Accuracy"))
